d73ac9e94744db35a918f63b2fb6f7c864a5458f
poojay6675 <78363317+poojay6675@users.noreply.github.com>
2022-05-30 09:44:06 +0530

    Update CreateStory.js

--- screens/CreateStory.js
+++ screens/CreateStory.js
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
 export default class CreateStory extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Create Story</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Create Story</Text>
+            </View>
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
